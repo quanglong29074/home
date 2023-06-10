5c8580ad84cec6657bffb8163c34cacc3de8e9c0
quanglong29074 <114908074+quanglong29074@users.noreply.github.com>
2023-06-10 14:37:44 +0700

    l

---
+++ src/controllers/HomeController.java
@@ -0,0 +1,75 @@
+package controllers;
+
+import enums.RepositoryType;
+import factory.RepositoryFactory;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import model.Table;
+
+import javax.swing.table.DefaultTableModel;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+    public TableView<Table> tbv;
+    public TableColumn<Table, Integer> colID;
+    public TableColumn<Table, String> colName;
+    public TableColumn<Table, Integer> colQty;
+    public TableColumn<Table, Double> colPrice;
+    public Label qty1;
+    public Label total;
+
+    ObservableList<Table> list = FXCollections.observableArrayList();
+    public static Table btnDelete;
+    public void addTable(int id, int qty ,String name,  Double price) {
+        // product already add
+        colID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        Table tb = new Table(id, qty, name, price);
+        list.add(tb);
+        tbv.setItems(list);
+    }
+
+    public void addToTable(MouseEvent mouseEvent) throws Exception {
+      try {
+          int count = Integer.parseInt(qty1.getText());
+          count ++;
+          qty1.setText(String.valueOf(count));
+
+          Double price = Math.ceil((count * 3.99) * 100) / 100;
+          addTable(1, count,"Tocotoco", price);
+
+
+
+      }catch (Exception e) {
+          System.out.println("Error: " + e.getMessage());
+      }
+    }
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        colID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        try {
+//            ObservableList<Table> list = FXCollections.observableArrayList();
+            // query
+            list.addAll(RepositoryFactory.createRepositoryInstance(RepositoryType.TABLE).getAll());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
